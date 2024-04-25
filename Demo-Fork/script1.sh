d0e6e73582b603061cf9f2ed9ba125b329cc84fe
KonstantinChuper <konstantyn.chuper@gmail.com>
2024-04-25 17:35:53 +0200

    Initialized a new repositoryI

---
+++ Demo-Fork/script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NOW=$(date)
+NAME="Konstantin"
+GROUP="WDM-290124"
+echo "Now is $NOW"
+echo "My name is $NAME. My group is $GROUP"
+
+
+
+
+
+echo "Done!"
---
+++ Homework-12/README.md
@@ -0,0 +1,3 @@
+# Homework-12
+Homework-12
+This is a description of this repository
